aba8f6fa7714f67aaee519b23a58ed9cb874d719
Garrett.Partenza <garrett.partenza@GPartenza-Mac.attlocal.net>
2024-05-21 15:51:17 -0400

    implemented long division

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,14 @@ impl<T> Polynomial<T> where T: Num + Clone, {
             .collect();
         Polynomial {terms}
     }
+
+    fn degree(&self) -> usize {
+        self.terms
+            .iter()
+            .filter_map(|term| if term.coefficient != T::zero() { Some(term.degree) } else { None })
+            .max()
+            .unwrap_or(0) 
+    }
 }
 
 impl<T: fmt::Display> fmt::Display for Term<T> {
@@ -49,6 +57,14 @@ impl<T: fmt::Display> fmt::Display for Polynomial<T> {
             }
         }
         Ok(())
+    }
+}
+
+impl<T> ops::Div<Term<T>> for Term<T> where T: Num + Clone {
+    type Output = Term<T>;
+
+    fn div(self, rhs: Term<T>) -> Term<T> {
+        Term::new(self.coefficient / rhs.coefficient, self.degree - rhs.degree)
     }
 }
 
@@ -117,15 +133,49 @@ impl<T> ops::Mul<Polynomial<T>> for Polynomial<T> where T: Num + Clone {
     }
 }
 
+impl<T> ops::Div<Polynomial<T>> for Polynomial<T> where T: Num + Clone + std::fmt::Display {
+    type Output = (Polynomial<T>, Polynomial<T>);
+
+    fn div(self, rhs: Polynomial<T>) -> (Polynomial<T>, Polynomial<T>) {
+
+        let mut Q = Polynomial::new(vec![T::zero(); rhs.terms.len()]);
+
+        let mut N = self.clone();
+        let mut D = rhs.clone();
 
 
+        println!("D {}", N);
+        println!("N {}", D);
+        println!("Q {}", Q);
 
+        while N.degree() >= D.degree() {
+            println!("Q {}", Q);
+            let mut d_terms = D.clone().terms;
+            d_terms.rotate_right(N.degree() - D.degree());
+            let mut d = Polynomial::new(d_terms.into_iter().map(|t| t.coefficient).collect());
+            println!("d {}", d);
+            Q.terms[N.degree() - D.degree()] = N.terms[N.degree()].clone() / d.terms[D.degree()].clone();
+            let mut factor = Polynomial::new(vec![T::zero(); rhs.terms.len()]);
+            factor.terms[N.degree() - D.degree()] = Q.terms[N.degree() - D.degree()].clone();
+            d = d.clone() * factor.clone();
+            N = N.clone() - d.clone();
+            println!("N (new) {}", N);
+            println!("Factor (new) {}", factor.clone());
+        }
+
+        (Q, N.clone())
+    }
+}
 
 fn main() {
     let poly1 = Polynomial::new(vec![1, 2, 3]);
     let poly2 = Polynomial::new(vec![1, 2, 3]);
-    println!("Addition: {}", poly1.clone() + poly2.clone());
-    println!("Subtraction: {}", poly1.clone() - poly2.clone());
-    println!("Multiplication: {}", poly1.clone() * poly2.clone());
-    println!("{}", Term::new(2, 3) * Term::new(2, 3));
+    //println!("Addition: {}", poly1.clone() + poly2.clone());
+    //println!("Subtraction: {}", poly1.clone() - poly2.clone());
+    //println!("Multiplication: {}", poly1.clone() * poly2.clone());
+    let (q, r) = poly1.clone() / poly2.clone();
+    println!("Division: q={}, r={}", q, r);
+    //println!("{}", Term::new(2, 3) * Term::new(2, 3));
+    //println!("Degree: {}", poly1.clone().degree());
+    //println!("Term Division 9x**4 / 3x**2: {}", Term::new(9, 4) / Term::new(3, 2));
 }
